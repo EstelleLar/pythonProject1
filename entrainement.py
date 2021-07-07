4f84c49ed7bf36eceff429df8e4068999abfd9fd
Estelle <estelle.laroche1@campus-igs-toulouse.fr>
2021-07-07 09:39:53 +0200

    ajout exercice list wikibook

--- entrainement.py
+++ entrainement.py
@@ -1,2 +1,10 @@
 # -*-coding:Utf-8 -*
-print ("hello")
+from random import *
+#wikibooks list exo 1
+s = []
+tailleliste = 9
+for i in range(tailleliste):
+    s.append(random())
+    # possible d'un print intermediaire pour voir la liste ce construire
+print (s)
+# on a 9 éléments rajouter dans la liste
